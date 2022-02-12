5150e7c110742abb4f77d6e55ae9b1443c259deb
Julien Castillo <juliencastillo77@gmail.com>
2022-02-12 17:51:11 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang='fr'>
+<head>
+<meta charset='UTF-8' />
+<meta http-equiv='X-UA-Compatible' content='IE=edge' />
+<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+<link rel='stylesheet' href='design/style.css' />
+<title>Espace Membre</title>
+</head>
+<body>
+
+</body>
+<script src='text/javascript' href='App.js'></script>
+</html>
